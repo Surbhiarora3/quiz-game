72ecc9407304f5f37a26754140a0b897b3645755
Surbhi Arora <arora.surbhi4@gmail.com>
2022-04-05 01:25:34 -0700

    added style and questions

--- css.js
+++ css.js
@@ -1,31 +1,131 @@
-// let button= document.querySelector("button")
-// console.log= "button"
+const quiz=[
+ {   
+question:"Question1: What is HTML?",
+a: "Hello to some language",
+b: "helium toy made language",
+c: "Hypertext Makeup Language",
+d: "Hypertext Markup Language",
 
-// button.addEventListener("click",()=> {
-//     alert("lets start the Game")
+ans: "ans4"
+ },
+{
+question:"Question2: what is CSS?",
+a: "Camel Style Casing",
+b: "Cascading Style Sheet",
+c: "Camel Style Sheet",
+d: "Case Study Sheet",
 
-// });
+ans: "ans2"
+},
+ 
+{
+ question:"Question3: Which heading size is the largest?",
+a: "h1",
+b: "h2",
+c: "h3",
+d: "h4",
 
+ans: "ans1"
+},
 
-//  var question1 = document.getElementById("p");
-// // var question1 = document.createElement("ready for first question");
-// document.body.appendchild(question1);
-// {/* <p id="p"> what is your answer
-// </p>; */}
+{
+    question:"Question4: h1 and h3 are examples of what?",
+a: "Heading",
+b: "paragraph  ",
+c: "Address",
+d: "styling",
 
-
-
-
-// // let button = document.querySelector("button")
-// // console.log (button);
-var words =["Surbhi", "Aryan", "Aarohi", "Sumit"]
-var choosenword=words[Math.floor(Math.random()*words.length)];
-choosenword = choosenword.split("")
-console.log("choosenword", choosenword)
-var userGuessarray=[];
-for (let i =0; i<choosenword.length;i++){
-    userGuessarray.push("")
+ans: "ans1"
 }
-console.log("userguess:" , userGuessarray)
-wordpara.textcontent = "us"
+];
+
+const question = document.querySelector('.question');
+const option1 = document.querySelector('#option1');
+const option2 = document.querySelector('#option2');
+const option3 = document.querySelector('#option3');
+const option4 = document.querySelector('#option4');
+const click = document.querySelectorAll('.click');
+
+const answers = document.querySelectorAll('.answer');
+
+ const showscore = document.querySelector('#showscore');
+let questionCount= 0;
+let score = 0;
+
+const loadQuestion = () => {
+
+    const questionList = quiz[questionCount];
+
+  question.innerHTML=  questionList.question;
+
+  option1.innerHTML = questionList.a;
+  option2.innerHTML = questionList.b;
+  option3.innerHTML = questionList.c;
+  option4.innerHTML = questionList.d;
+
+}
+
+loadQuestion();
+
+const getCheckAnswer = () => {
+let answer;
+answers.forEach((currentAnswerElement)=>{
+    if(currentAnswerElement.checkede){
+        answer = currentAnswerElement.id;
+    }
+    
+});
+
+return answer;
+
+};
+
+const deselectAll=()=>{
+    answers.forEach((currentAnswerElement)=> currentAnswerElement.checkedAnswer =false );
+}
+  
+
+submit.addEventListener ('click',() => {
+    const CheckedAnswer = getCheckAnswer();
+    console.log(CheckedAnswer);
+
+    if (CheckedAnswer == quiz [questionCount].ans){
+        score++;
+    };
+    questionCount++;
+
+    if (questionCount <quiz.length){
+        loadQuestion();
+    }
+    else{
+        showscore.innerHTML= score;
+
+        // <h3>you scored ${} /${quiz.length}</h3>
+    }
+
+
+    showscore.classList.remove('scoreArea');
+
+});
+
+function Timer()
+    {
+         alert("You are out of time!");
+    }
+
+
+
+
+
+
+// var words =["Surbhi", "Aryan", "Aarohi", "Sumit"]
+// var choosenword=words[Math.floor(Math.random()*words.length)];
+// choosenword = choosenword.split("")
+// console.log("choosenword", choosenword)
+// var userGuessarray=[];
+// for (let i =0; i<choosenword.length;i++){
+//     userGuessarray.push("")
+// }
+// console.log("userguess:" , userGuessarray)
+// wordpara.textcontent = "us"
 
